8ad60d3203c8f266cf8bb4bf21c1ffe7fa202f36
Andrej-ori <107578974+Andrej-ori@users.noreply.github.com>
2022-08-01 20:56:56 +0300

    Update BonusServiceTest.java

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -3,14 +3,14 @@ package ru.netology.bonus;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusServiceTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv")
+    @CsvFileSource(resources = "/data.csv", delimiter = ';')
 
-    void shouldCalculateForRegisteredAndUnderLimit(long amount, boolean registered, long expected) {
+    void shouldCalculateForRegisteredAndUnderLimit(String testName, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
 
         long actual = service.calculate(amount, registered);
